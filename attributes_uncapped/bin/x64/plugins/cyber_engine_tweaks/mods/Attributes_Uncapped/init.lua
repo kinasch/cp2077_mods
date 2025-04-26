1418ca6bdf6223c164b28dec276972de97ae3d4e
kinasch <janik@schoenberg.name>
2025-04-26 03:24:01 +0200

    attr_uncap: init, track attribute passive beyond level 20#+

---
+++ attributes_uncapped/bin/x64/plugins/cyber_engine_tweaks/mods/Attributes_Uncapped/init.lua
@@ -0,0 +1,83 @@
+-- Init
+registerForEvent("onInit", function()
+	old_value = {
+		Reflexes=0,
+		Intelligence=0,
+		TechnicalAbility=0,
+		Strength=0,
+		Cool=0
+	}
+
+	-- Raise max value of every attribute.
+	for key, value in pairs(old_value) do
+		TweakDB:SetFlat("BaseStats."..key..".max",99)
+	end
+
+	-- Set up the stat modifiers for attribute levels beyond 20.
+	stat_modifier = {
+		Reflexes=function(amount)
+			return RPGManager.CreateStatModifier(gamedataStatType.CritChance, gameStatModifierType.Additive, amount * 0.5)
+		end,
+		Intelligence=function(amount)
+			if amount < 0 then
+				amount = math.ceil(amount/4)
+			else
+				amount = math.floor(amount/4)
+			end
+			return RPGManager.CreateStatModifier(gamedataStatType.Memory, gameStatModifierType.Additive, amount * 1)
+		end,
+		TechnicalAbility=function(amount)
+			return RPGManager.CreateStatModifier(gamedataStatType.Armor, gameStatModifierType.Additive, amount * 2)
+		end,
+		Strength=function(amount)
+			return RPGManager.CreateStatModifier(gamedataStatType.Health, gameStatModifierType.Additive, amount * 2)
+		end,
+		Cool=function(amount)
+			return RPGManager.CreateStatModifier(gamedataStatType.CritDamage, gameStatModifierType.Additive, amount * 1.25)
+		end
+	}
+
+	-- Observe the SetAttribute function of PlayerDevelopmentData, as it is called by BuyAttribute and ModifyAttribute
+	-- There's (almost) no change of the attributes without it.
+	-- Get the old value(s) before changing / setting / buying attributes to compare them later on.
+	Observe("PlayerDevelopmentData", "SetAttribute",function(this, type, amount)
+		type = type.value
+		old_value[type] = PlayerDevelopmentSystem.GetData(GetPlayer()):GetAttributeValue(type)
+	end)
+
+	-- Check the new amount for actions beyond the normal 
+	ObserveAfter("PlayerDevelopmentData", "SetAttribute",function(this, type, amount)
+		type = type.value
+		-- Check for resetting or other, more unusual, attribute modifications
+		if amount < old_value[type] then
+			Game.GetStatsSystem():AddModifier(
+				GetPlayer():GetEntityID(),
+				stat_modifier[type]( - old_value[type] + Max(amount,20) )
+			)
+		-- Levels 1 through 20, the game handles the attribute passives itself, beyond that it doesn't, no matter the max.
+		-- Tbf, this whole mod would be useless if that was not the case.
+		elseif amount > 20 then
+			Game.GetStatsSystem():AddModifier(
+				GetPlayer():GetEntityID(),
+				stat_modifier[type](amount-old_value[type])
+			)
+		end
+	end)
+
+	-- Get attribute levels after loading in to avoid cheesing the mod by reloading all mods and stacking modifiers.
+	-- This is getting called like 200 times, but whatever...
+	ObserveAfter("PlayerDevelopmentData", "OnRestored",function(this, gameInstance)
+		for key, value in pairs(old_value) do
+			cur_level = PlayerDevelopmentSystem.GetData(GetPlayer()):GetAttributeValue(gamedataStatType[key])
+			if cur_level > 20 then
+				Game.GetStatsSystem():AddModifier(
+					GetPlayer():GetEntityID(),
+					stat_modifier[key](Max(0,cur_level-old_value[key]-20))
+				)
+			end
+
+			-- Could one do `value = ...` here? idk
+			old_value[key] = cur_level
+		end
+	end)
+end)
---
+++ attributes_uncapped/r6/scripts/Attributes_Uncapped/attributes_uncapped.reds
@@ -0,0 +1,20 @@
+@replaceMethod(PlayerDevelopmentDataManager)
+private final func FillAttributeData(attribute: SAttribute, out outData: ref<AttributeData>) -> Void {
+	if Equals(attribute.attributeName, gamedataStatType.Espionage) {
+	  outData.label = "LocKey#20997";
+	} else {
+	  outData.label = TweakDBInterface.GetStatRecord(TDBID.Create("BaseStats." + EnumValueToString("gamedataStatType", Cast<Int64>(EnumInt(attribute.attributeName))))).LocalizedName();
+	};
+	outData.value = attribute.value;
+	outData.maxValue = 99;
+	outData.id = attribute.id;
+	outData.availableToUpgrade = outData.value < outData.maxValue;
+	outData.type = attribute.attributeName;
+	outData.description = TweakDBInterface.GetStatRecord(TDBID.Create("BaseStats." + EnumValueToString("gamedataStatType", Cast<Int64>(EnumInt(attribute.attributeName))))).LocalizedDescription();
+}
+
+@wrapMethod(PerkMenuTooltipController)
+public func SetData(tooltipData: ref<ATooltipData>) -> Void {
+  this.m_maxProficiencyLevel = 99;
+  wrappedMethod(tooltipData);
+}
