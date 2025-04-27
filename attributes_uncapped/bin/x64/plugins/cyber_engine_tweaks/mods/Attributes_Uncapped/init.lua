213b50b92915c2c0d0b57e2ce10182db506f3836
kinasch <janik@schoenberg.name>
2025-04-27 16:28:20 +0200

    attr_uncap: removed nil error on game start

--- attributes_uncapped/bin/x64/plugins/cyber_engine_tweaks/mods/Attributes_Uncapped/init.lua
+++ attributes_uncapped/bin/x64/plugins/cyber_engine_tweaks/mods/Attributes_Uncapped/init.lua
@@ -1,5 +1,7 @@
 -- Init
 registerForEvent("onInit", function()
+	local loaded = false
+
 	local old_value = {
 		Reflexes=0,
 		Intelligence=0,
@@ -47,12 +49,14 @@ registerForEvent("onInit", function()
 	-- There's (almost) no change of the attributes without it.
 	-- Get the old value(s) before changing / setting / buying attributes to compare them later on.
 	Observe("PlayerDevelopmentData", "SetAttribute",function(this, type, amount)
+		--if not loaded then return end
 		type = type.value
 		old_value[type] = PlayerDevelopmentSystem.GetData(GetPlayer()):GetAttributeValue(type)
 	end)
 
 	-- Check the new amount for actions beyond the normal 
 	ObserveAfter("PlayerDevelopmentData", "SetAttribute",function(this, type, amount)
+		if not loaded then return end
 		type = type.value
 		-- Check for resetting or other, more unusual, attribute modifications
 		if amount < old_value[type] then
@@ -71,7 +75,7 @@ registerForEvent("onInit", function()
 				
 			end)
 			if not worked then
-				spdlog.warning("Might be a nil error from startup:\n"..tostring(error))
+				spdlog.warning("Error, Set Attribute amount<old_value:\n"..tostring(error))
 			end
 		-- Levels 1 through 20, the game handles the attribute passives itself, beyond that it doesn't, no matter the max.
 		-- Tbf, this whole mod would be useless if that was not the case.
@@ -90,7 +94,7 @@ registerForEvent("onInit", function()
 				end
 			end)
 			if not worked then
-				spdlog.warning("Might be a nil error from startup:\n"..tostring(error))
+				spdlog.warning("Error, Set Attribute amount>20:\n"..tostring(error))
 			end
 		end
 	end)
@@ -98,6 +102,8 @@ registerForEvent("onInit", function()
 	-- Get attribute levels after loading in to avoid cheesing the mod by reloading all mods and stacking modifiers.
 	-- This is hopefully only getting called once.
 	ObserveAfter("PlayerDevelopmentSystem", "OnRestored",function(this, saveVersion, gameVersion)
+		loaded = true
+
 		for key, value in pairs(old_value) do
 			local cur_level = PlayerDevelopmentSystem.GetData(GetPlayer()):GetAttributeValue(gamedataStatType[key])
 			if cur_level > 20 then
