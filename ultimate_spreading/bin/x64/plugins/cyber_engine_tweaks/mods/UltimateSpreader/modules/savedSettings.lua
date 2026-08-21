da79cfbccbf5b8bc5fc9b49fa8ee99ac421c541c
kinasch <janik@schoenberg.name>
2026-08-21 04:09:05 +0200

    ultspread: updated alternative CET version based on vanilla spread

--- ultimate_spreading/bin/x64/plugins/cyber_engine_tweaks/mods/UltimateSpreader/modules/savedSettings.lua
+++ ultimate_spreading/bin/x64/plugins/cyber_engine_tweaks/mods/UltimateSpreader/modules/savedSettings.lua
@@ -1,174 +1,20 @@
 -- Default values are taken from the respective TweakDB defaults
 savedSettings = {
+    -- TODO: Some defaults are wrong
     settings = {
-        suicide = {
-            count = {
-                default = 1,
-                saved = 1
-            },
-            bonusJumps = {
-                default = 0,
-                saved = 0
-            },
-            range = {
-                default = -1,
-                saved = -1
-            },name = "Suicide"
-        },
-        systemCollapse = {
-            count = {
-                default = 1,
-                saved = 1
-            },
-            bonusJumps = {
-                default = 0,
-                saved = 0
-            },
-            range = {
-                default = -1,
-                saved = -1
-            },name = "SystemCollapse"
-        },
-        contagion = {
-            count = {
-                default = -1,
-                saved = -1
-            },
-            bonusJumps = {
-                default = 1,
-                saved = 1
-            },
-            range = {
-                default = -1,
-                saved = -1
-            },name = "Contagion"
-        },
-        overheat = {
-            count = {
-                default = 2,
-                saved = 2
-            },
-            bonusJumps = {
-                default = 0,
-                saved = 0
-            },
-            range = {
-                default = 6,
-                saved = 6
-            },name = "Overheat"
-        },
-        blind = {
-            count = {
-                default = -1,
-                saved = -1
-            },
-            bonusJumps = {
-                default = 0,
-                saved = 0
-            },
-            range = {
-                default = -1,
-                saved = -1
-            },name = "Blind"
-        },
-        cyberwareMalfunction = {
-            count = {
-                default = -1,
-                saved = -1
-            },
-            bonusJumps = {
-                default = 0,
-                saved = 0
-            },
-            range = {
-                default = -1,
-                saved = -1
-            },name = "CyberwareMalfunction"
-        },
-        locomotionMalfunction = {
-            count = {
-                default = -1,
-                saved = -1
-            },
-            bonusJumps = {
-                default = 0,
-                saved = 0
-            },
-            range = {
-                default = -1,
-                saved = -1
-            },name = "LocomotionMalfunction"
-        },
-        weaponMalfunction = {
-            count = {
-                default = -1,
-                saved = -1
-            },
-            bonusJumps = {
-                default = 0,
-                saved = 0
-            },
-            range = {
-                default = -1,
-                saved = -1
-            },name = "WeaponMalfunction"
-        },
-        grenade = {
-            count = {
-                default = -1,
-                saved = -1
-            },
-            bonusJumps = {
-                default = 0,
-                saved = 0
-            },
-            range = {
-                default = -1,
-                saved = -1
-            },name = "Grenade"
-        },
-        overload = {
-            count = {
-                default = 2,
-                saved = 2
-            },
-            bonusJumps = {
-                default = 0,
-                saved = 0
-            },
-            range = {
-                default = 6,
-                saved = 6
-            },name = "Overload"
-        },
-        brainMelt = {
-            count = {
-                default = 2,
-                saved = 2
-            },
-            bonusJumps = {
-                default = 0,
-                saved = 0
-            },
-            range = {
-                default = 6,
-                saved = 6
-            },name = "BrainMelt"
-        },
-        madness = {
-            count = {
-                default = -1,
-                saved = -1
-            },
-            bonusJumps = {
-                default = 0,
-                saved = 0
-            },
-            range = {
-                default = -1,
-                saved = -1
-            },name = "Madness"
-        },
+        overload = { count = { default = 0, saved = 0 }, bonusJumps = { default = 0, saved = 0 }, range = { default = -1, saved = -1 }, name = "Overload" },
+        overheat = { count = { default = 0, saved = 0 }, bonusJumps = { default = 0, saved = 0 }, range = { default = -1, saved = -1 }, name = "Overheat" },
+        contagion = { count = { default = -1, saved = -1 }, bonusJumps = { default = 0, saved = 0 }, range = { default = -1, saved = -1 }, name = "Contagion" },
+        brainMelt = { count = { default = 0, saved = 0 }, bonusJumps = { default = 0, saved = 0 }, range = { default = -1, saved = -1 }, name = "BrainMelt" },
+        blind = { count = { default = 0, saved = 0 }, bonusJumps = { default = 0, saved = 0 }, range = { default = -1, saved = -1 }, name = "Blind" },
+        weaponMalfunction = { count = { default = 0, saved = 0 }, bonusJumps = { default = 0, saved = 0 }, range = { default = -1, saved = -1 }, name = "WeaponMalfunction" },
+        locomotionMalfunction = { count = { default = 0, saved = 0 }, bonusJumps = { default = 0, saved = 0 }, range = { default = -1, saved = -1 }, name = "LocomotionMalfunction" },
+        cyberwareMalfunction = { count = { default = 0, saved = 0 }, bonusJumps = { default = 0, saved = 0 }, range = { default = -1, saved = -1 }, name = "CyberwareMalfunction" },
+        suicide = { count = { default = 0, saved = 0 }, bonusJumps = { default = 0, saved = 0 }, range = { default = -1, saved = -1 }, name = "Suicide" },
+        madness = { count = { default = 0, saved = 0 }, bonusJumps = { default = 0, saved = 0 }, range = { default = -1, saved = -1 }, name = "Madness" },
+        grenade = { count = { default = 0, saved = 0 }, bonusJumps = { default = 0, saved = 0 }, range = { default = -1, saved = -1 }, name = "Grenade" },
+        systemCollapse = { count = { default = 0, saved = 0 }, bonusJumps = { default = 0, saved = 0 }, range = { default = -1, saved = -1 }, name = "SystemCollapse" },
+        blackWall = { count = { default = -1, saved = -1 }, bonusJumps = { default = 0, saved = 0 }, range = { default = 20, saved = 20 }, name = "BlackWall" }
     }
 }
 
@@ -184,7 +30,7 @@ function savedSettings.tryToLoadSettings()
         fileFunctions.load()
         return true
     else
-        print("No saved settings file found. Creating one ...")
+        --print("No saved settings file found. Creating one ...")
         fileFunctions.save()
         return false
     end
@@ -196,7 +42,7 @@ function savedSettings.tryToSaveSettings()
         io.close(file)
         fileFunctions.save()
     else
-        print("No saved settings file found. Creating one ...")
+        --print("No saved settings file found. Creating one ...")
         fileFunctions.save()
     end
 end
