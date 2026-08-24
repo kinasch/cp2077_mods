e54eb9c668fe4c0cb89e32b92555aa8308661d92
kinasch <janik@schoenberg.name>
2026-08-24 22:50:13 +0200

    crit_conv: added detection of changes to weapon crit chance, added applying these to the player and unapplying them for various circumstances

--- crit_conversion/bin/x64/plugins/cyber_engine_tweaks/mods/CritConversion/init.lua
+++ crit_conversion/bin/x64/plugins/cyber_engine_tweaks/mods/CritConversion/init.lua
@@ -1,4 +1,7 @@
 local ratio = 1
+---@type StatsObjectID
+local currentEquippedWeaponStatsObjectID = nil;
+local equippedWeaponCritChance = 0.0
 
 registerForEvent("onTweak", function()
     -- Set the maximum crit chance to some arbritary high number, early!
@@ -6,6 +9,12 @@ registerForEvent("onTweak", function()
 end)
 
 registerForEvent("onInit", function()
+    -- Get equipped weapon (if mod is reloaded during gameplay)
+    local curEquippedWeapon = GetPlayer().equippedRightHandWeapon
+    if curEquippedWeapon ~= nil then
+        currentEquippedWeaponStatsObjectID = curEquippedWeapon:GetItemData():GetStatsObjectID()
+    end
+
     -- Observe the player for stat changes related to crit chance
     -- Will miss things like bonus crit chance, no idea about the impact of this though
     Observe("PlayerPuppet", "OnStatChanged",function(this, ownerID, statType, diff, total)
@@ -27,6 +36,154 @@ registerForEvent("onInit", function()
                     RPGManager.CreateStatModifier(gamedataStatType.CritDamage, gameStatModifierType.Additive, -ratio*(total-diff-100))
                 );
 			end
+            print("Player Crit Chance:", total,"- Player Crit Dmg:", Game.GetStatsSystem():GetStatValue(Game.GetPlayer():GetEntityID(), gamedataStatType.CritDamage))
         end
     end)
+
+    -- This and the observer above mean, that this mod adds A LOT of modifiers to the player, might decrease performance, idk.
+    Observe("PlayerPuppet", "OnItemEquipped", function(this, slot, item)
+        if slot==TweakDB:GetRecord("AttachmentSlots.WeaponRight"):GetID() then
+            currentEquippedWeaponStatsObjectID = Game.GetTransactionSystem():GetItemData(GetPlayer(), item):GetStatsObjectID()
+            print(currentEquippedWeaponStatsObjectID)
+            equippedWeaponCritChance = Game.GetStatsSystem():GetStatValue(currentEquippedWeaponStatsObjectID, gamedataStatType.CritChance)
+            Game.GetStatsSystem():AddModifier(Game.GetPlayer():GetEntityID(), RPGManager.CreateStatModifier(gamedataStatType.CritChance, gameStatModifierType.Additive, equippedWeaponCritChance))
+        end    
+    end)
+    Observe("PlayerPuppet", "OnItemUnequipped", function(this, slot, item)
+        if slot==TweakDB:GetRecord("AttachmentSlots.WeaponRight"):GetID() then
+            --Game.GetStatsSystem():AddModifier(Game.GetPlayer():GetEntityID(), RPGManager.CreateStatModifier(gamedataStatType.CritChance, gameStatModifierType.Additive, -equippedWeaponCritChance))
+            --Game.GetStatsSystem():RemoveModifier(Game.GetPlayer():GetEntityID(), RPGManager.CreateStatModifier(gamedataStatType.CritChance, gameStatModifierType.Additive, equippedWeaponCritChance))
+            for _, detailedStatData in pairs(Game.GetStatsSystem():GetStatDetails(currentEquippedWeaponStatsObjectID)) do
+                if detailedStatData.statType == gamedataStatType.CritChance then
+                    for _, detailedStat in pairs(detailedStatData.modifiers) do
+                        Game.GetStatsSystem():RemoveModifier(Game.GetPlayer():GetEntityID(), RPGManager.CreateStatModifier(gamedataStatType.CritChance, gameStatModifierType.Additive, detailedStat.value))
+                    end
+                end
+            end
+            currentEquippedWeaponStatsObjectID = nil
+            print("unequipped weapon: ",currentEquippedWeaponStatsObjectID==nil)
+            equippedWeaponCritChance = 0.0
+        end
+    end)
+
+    -- Observe for stats changing on equipped weapon
+    -- Added modifiers
+    ObserveAfter("StatsSystem", "AddModifier", function(this, objID, modifierData)
+        processWeaponStatChanged(objID, modifierData.statType, "AddModifier", false)
+    end)
+    ObserveAfter("StatsSystem", "AddModifiers", function(this, objID, modifierData)
+        for _, modData in pairs(modifierData) do
+            processWeaponStatChanged(objID, modData.statType, "AddModifiers", false)
+        end
+    end)
+    ObserveAfter("StatsSystem", "AddSavedModifier", function(this, objID, modifierData)
+        processWeaponStatChanged(objID, modifierData.statType, "AddSavedModifier", false)
+    end)
+    ObserveAfter("StatsSystem", "ApplyModifierGroup", function(this, objID, groupID)
+        -- just pass the right statType, increases comparisons done and is not even remotely efficient, but I do not know how to get from the groupID to its statModifiers
+        processWeaponStatChanged(objID, gamedataStatType.CritChance, "ApplyModifierGroup", false)
+    end)
+    -- ForceModifier here as well? idk - needs testing
+
+    -- Removed Modifiers
+    ObserveAfter("StatsSystem", "RemoveModifier", function(this, objID, modifierData)
+        processWeaponStatChanged(objID, modifierData.statType, "RemoveModifier", true)
+    end)
+    ObserveAfter("StatsSystem", "RemoveAllModifiers", function(this, objID, statType, removeSavedModifiers)
+        processWeaponStatChanged(objID, statType, "RemoveAllModifiers", true)
+    end)
+    ObserveAfter("StatsSystem", "RemoveSavedModifiers", function(this, objID, modifierData)
+        processWeaponStatChanged(objID, modifierData.statType, "RemoveSavedModifiers", true)
+    end)
+    ObserveAfter("StatsSystem", "RemoveModifierGroup", function(this, objID, groupID)
+        -- just pass the right statType, increases comparisons done and is not even remotely efficient, but I do not know how to get from the groupID to its statModifiers
+        processWeaponStatChanged(objID, gamedataStatType.CritChance, "RemoveModifierGroup", true)
+    end)
+    ObserveAfter("StatsSystem", "RemoveAndUncacheModifier", function(this, objID, modifierData)
+        processWeaponStatChanged(objID, modifierData.statType, "RemoveAndUncacheModifier", true)
+    end)
+    -- UnforceStat here as well? idk - needs testing
+
+
+    -- Check the crit chance calculation for quickhack and remove weapon crit chance for this
+    Observe("DamageSystem", "ProcessCriticalHit",
+    ---@param this DamageSystem
+    ---@param hitEvent gameHitEvent
+    function(this, hitEvent)
+        if hitEvent.attackData:GetAttackType() == gamedataAttackType.Hack or hitEvent.attackData:HasFlag(hitFlag.QuickHack) then
+            print("Hack, removing weapon crit chance")
+            for _, detailedStatData in pairs(Game.GetStatsSystem():GetStatDetails(currentEquippedWeaponStatsObjectID)) do
+                if detailedStatData.statType == gamedataStatType.CritChance then
+                    for _, detailedStat in pairs(detailedStatData.modifiers) do
+                        Game.GetStatsSystem():RemoveModifier(Game.GetPlayer():GetEntityID(), RPGManager.CreateStatModifier(gamedataStatType.CritChance, gameStatModifierType.Additive, detailedStat.value))
+                    end
+                end
+            end
+            --Game.GetStatsSystem():AddModifier(Game.GetPlayer():GetEntityID(), RPGManager.CreateStatModifier(gamedataStatType.CritChance, gameStatModifierType.Additive, -equippedWeaponCritChance))
+            print(Game.GetStatsSystem():GetStatValue(GetPlayer():GetEntityID(), gamedataStatType.CritChance))
+            for _, detailedStatData in pairs(Game.GetStatsSystem():GetStatDetails(GetPlayer():GetEntityID())) do
+                if detailedStatData.statType == gamedataStatType.CritChance then
+                    print(#detailedStatData.modifiers)
+                end
+            end
+        end
+    end)
+     ObserveAfter("DamageSystem", "ProcessCriticalHit",
+    ---@param this DamageSystem
+    ---@param hitEvent gameHitEvent
+    function(this, hitEvent)
+        if hitEvent.attackData:GetAttackType() == gamedataAttackType.Hack or hitEvent.attackData:HasFlag(hitFlag.QuickHack) then
+            print("Hack done, readding weapon crit chance")
+            for _, detailedStatData in pairs(Game.GetStatsSystem():GetStatDetails(currentEquippedWeaponStatsObjectID)) do
+                if detailedStatData.statType == gamedataStatType.CritChance then
+                    for _, detailedStat in pairs(detailedStatData.modifiers) do
+                        Game.GetStatsSystem():AddModifier(Game.GetPlayer():GetEntityID(), RPGManager.CreateStatModifier(gamedataStatType.CritChance, gameStatModifierType.Additive, detailedStat.value))
+                    end
+                end
+            end
+            --Game.GetStatsSystem():AddModifier(Game.GetPlayer():GetEntityID(), RPGManager.CreateStatModifier(gamedataStatType.CritChance, gameStatModifierType.Additive, equippedWeaponCritChance))
+        end
+        -- Also add the additional crit chance to player crit chance (to re-calculate crit damage)
+        if hitEvent.attackData:GetAdditionalCritChance() > 0.0 then
+            print("Add additional crit chances for calc")
+            Game.GetStatsSystem():AddModifier(Game.GetPlayer():GetEntityID(), RPGManager.CreateStatModifier(gamedataStatType.CritChance, gameStatModifierType.Additive, hitEvent.attackData:GetAdditionalCritChance()))
+        end
+    end)
+
+    -- Remove additional crit chances after crit damage calc is done
+    ObserveAfter("DamageSystem", "GetCritDamageModifier",
+    ---@param this DamageSystem
+    ---@param statSystem StatsSystem
+    ---@param attackData AttackData
+    function(this, statSystem, attackData)
+        if attackData:GetAdditionalCritChance() > 0.0 then
+            print("Remove additional crit chances after calc")
+            Game.GetStatsSystem():AddModifier(Game.GetPlayer():GetEntityID(), RPGManager.CreateStatModifier(gamedataStatType.CritChance, gameStatModifierType.Additive, -hitEvent.attackData:GetAdditionalCritChance()))
+        end
+    end)
+
+
 end)
+
+---@param statsObjectID StatsObjectID
+---@param statType gamedataStatType
+---@param source? string
+---@param remove? boolean
+function processWeaponStatChanged(statsObjectID, statType, source, remove)
+    source = source or "UNKNOWN"
+    remove = remove or false
+    if currentEquippedWeaponStatsObjectID ~= nil then
+        if statType == gamedataStatType.CritChance and statsObjectID.entityHash == currentEquippedWeaponStatsObjectID.entityHash then
+            local weaponCritChance = Game.GetStatsSystem():GetStatValue(currentEquippedWeaponStatsObjectID, gamedataStatType.CritChance)
+            if weaponCritChance ~= equippedWeaponCritChance then
+                print("Weapon Crit Chance changed, by:", source, "- weaponCritChance vs saved",weaponCritChance, equippedWeaponCritChance)
+                if remove then
+                    Game.GetStatsSystem():RemoveModifier(Game.GetPlayer():GetEntityID(), RPGManager.CreateStatModifier(gamedataStatType.CritChance, gameStatModifierType.Additive, math.abs(weaponCritChance-equippedWeaponCritChance)))
+                else
+                    Game.GetStatsSystem():AddModifier(Game.GetPlayer():GetEntityID(), RPGManager.CreateStatModifier(gamedataStatType.CritChance, gameStatModifierType.Additive, math.abs(weaponCritChance-equippedWeaponCritChance)))
+                end
+                equippedWeaponCritChance = weaponCritChance
+            end
+        end
+    end
+end
