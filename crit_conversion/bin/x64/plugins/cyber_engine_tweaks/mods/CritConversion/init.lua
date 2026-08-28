8c7d081e7fb8bea5f9ae5195847affe80d0457b0
kinasch <janik@schoenberg.name>
2026-08-28 19:04:01 +0200

    crit_conv: added weapon crit values to player stats page

--- crit_conversion/bin/x64/plugins/cyber_engine_tweaks/mods/CritConversion/init.lua
+++ crit_conversion/bin/x64/plugins/cyber_engine_tweaks/mods/CritConversion/init.lua
@@ -12,12 +12,29 @@ registerForEvent("onInit", function()
             local statsDetailCritChance = math.min(stat.valueF, 100.00)
             inkTextRef:SetText(this.m_StatValueRef, FloatToStringPrec(statsDetailCritChance, 2) + "%");
         end ]]
+        if stat.type == gamedataStatType.CritChance then
+            local weaponCritChance = 0
+            if GetPlayer().equippedRightHandWeapon ~= nil then
+                weaponCritChance = Game.GetStatsSystem():GetStatValue(GetPlayer().equippedRightHandWeapon:GetEntityID(), gamedataStatType.CritChance)
+            end
+            local statsDetailCritChance = stat.valueF + weaponCritChance
+            --print(this.StatValueRef, Dump(this.StatValueRef,true))
+            ---@diagnostic disable-next-line: missing-parameter
+            this.StatValueRef:SetText(FloatToStringPrec(statsDetailCritChance, 2) .. "%")
+        end
         if stat.type == gamedataStatType.CritDamage then
             local playerCritChance = Game.GetStatsSystem():GetStatValue(GetPlayer():GetEntityID(), gamedataStatType.CritChance)
-            local statsDetailCritDamage = stat.valueF
-            if playerCritChance > 100.0 then
-                statsDetailCritDamage = statsDetailCritDamage + (playerCritChance - 100.0)
+            local weaponCritChance, weaponCritDamage = 0, 0
+            if GetPlayer().equippedRightHandWeapon ~= nil then
+                weaponCritChance = Game.GetStatsSystem():GetStatValue(GetPlayer().equippedRightHandWeapon:GetEntityID(), gamedataStatType.CritChance)
+                weaponCritDamage = Game.GetStatsSystem():GetStatValue(GetPlayer().equippedRightHandWeapon:GetEntityID(), gamedataStatType.CritDamage)
             end
+            local statsDetailCritDamage = stat.valueF
+            local combinedCritChanceWeaponAndPlayer = playerCritChance + weaponCritChance
+            if combinedCritChanceWeaponAndPlayer > 100.0 then
+                statsDetailCritDamage = statsDetailCritDamage + (combinedCritChanceWeaponAndPlayer - 100.0)
+            end
+            statsDetailCritDamage = statsDetailCritDamage + weaponCritDamage
             --print(this.StatValueRef, Dump(this.StatValueRef,true))
             ---@diagnostic disable-next-line: missing-parameter
             this.StatValueRef:SetText(FloatToStringPrec(statsDetailCritDamage, 2) .. "%")
